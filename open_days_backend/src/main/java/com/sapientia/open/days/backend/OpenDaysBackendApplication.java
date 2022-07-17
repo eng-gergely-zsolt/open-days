ec7a05dd3172e985e6e6cf926bf1ab4e1ff6cacd
geergely-zsolt <56913290+geergely-zsolt@users.noreply.github.com>
2022-07-17 15:16:24 +0300

    Add files via upload

---
+++ open_days_backend/src/main/java/com/sapientia/open/days/backend/OpenDaysBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sapientia.open.days.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenDaysBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenDaysBackendApplication.class, args);
+	}
+
+}
---
+++ open_days_backend/src/test/java/com/sapientia/open/days/backend/OpenDaysBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sapientia.open.days.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenDaysBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
