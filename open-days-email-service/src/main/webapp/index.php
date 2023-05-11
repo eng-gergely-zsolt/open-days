7effd6f78d04c91e6d73c92f48ec09bc95be00d9
Zsolt <geergely.zsolt@gmail.com>
2023-05-11 20:23:18 +0300

    Email service: Delete email service

--- open-days-email-service/src/main/webapp/index.php
+++
@@ -1,3 +0,0 @@
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
