f2b7e4ba1d16a70ccb6d33be0192af4288fd4d59
Zsolt <eng.gergely.zsolt@gmail.com>
2024-08-02 19:44:08 +0300

    Rename folders

---
+++ open-days-frontend/lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+
+import 'package:open_days_frontend/l10n/l10n.dart';
+import 'package:open_days_frontend/theme/theme.dart';
+
+import './route_generator.dart';
+import './constants/page_routes.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
+
+  runApp(
+    ProviderScope(
+      child: MaterialApp(
+        supportedLocales: L10n.all,
+        initialRoute: initialRoute,
+        theme: CustomTheme.lightTheme,
+        onGenerateRoute: RouteGenerator.generateRoute,
+        localizationsDelegates: const [
+          AppLocalizations.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+        ],
+      ),
+    ),
+  );
+}
--- open_days_frontend/android/app/src/main/kotlin/com/example/open_days_frontend/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.open_days_frontend
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
